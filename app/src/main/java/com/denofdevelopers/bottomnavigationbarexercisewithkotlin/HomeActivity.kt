b0ce43adfff15244eb78b61bd5f6b16100bd0378
Aleksandar Gulevski <aleksandar.gulevski@haselt.com>
2020-04-01 13:18:01 +0200

    Implemented bottom navigation view with on click listeners

--- app/src/main/java/com/denofdevelopers/bottomnavigationbarexercisewithkotlin/HomeActivity.kt
+++ app/src/main/java/com/denofdevelopers/bottomnavigationbarexercisewithkotlin/HomeActivity.kt
@@ -2,11 +2,35 @@ package com.denofdevelopers.bottomnavigationbarexercisewithkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    Toast.makeText(this, "Home selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.photos -> {
+                    Toast.makeText(this, "Photos selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.more -> {
+                    Toast.makeText(this, "More selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> true
+            }
+        }
     }
 }
